8a909928b92a78576d5d117dc48e0663ad8ea8e9
fucchihub <144200649+fucchihub@users.noreply.github.com>
2023-10-09 05:06:46 +0000

    [ADD]ルーティングとindexビュー

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,6 +2,7 @@ class BooksController < ApplicationController
 
   def index
     @book = Book.new
+    @books = Book.all
   end
 
   def create
@@ -11,6 +12,7 @@ class BooksController < ApplicationController
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,21 @@
 Rails.application.routes.draw do
   get '/' => 'homes#top'
-  get '/books' => 'books#index'
-  post '/books' => 'books#create'
-  get 'books/show'
-  get 'books/edit'
-  #post 'lists' => 'lists#create'
+  resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
+#Prefix      Verb   URI Pattern                 Controller#Action
+#            GET    /              	            homes#top
+#books       GET    /books(.:format)            books#index
+#            POST   /books(.:format)            books#create
+#new_book    GET    /books/new(.:format)        books#new
+#edit_book   GET    /books/:id/edit(.:format)   books#edit
+#list        GET    /books/:id(.:format)        books#show
+#            PATCH  /books/:id(.:format)        books#update
+#            PUT    /books/:id(.:format)        books#update
+#            DELETE /books/:id(.:format)        books#destroy
+
+#get 'books' => 'books#index'
+  #post 'books' => 'books#create'
+  #get 'books/:id' => 'books#show',as: 'list'
+  #get 'books/edit'
